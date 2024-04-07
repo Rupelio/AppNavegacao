bcad5b4ff5c5f260d1bfc53c1a9cdc797832d61b
Gabriel Chioquetta <gabriel.obladen@gmail.com>
2024-04-07 17:27:37 -0300

    Update colors.tsx

--- src/consts/colors.tsx
+++ src/consts/colors.tsx
@@ -1,8 +1,8 @@
 const COLORS = {
   white: '#FFF',
   dark: '#000',
-  primary: '#F9813A',
-  secondary: '#fedac5',
+  primary: '#4560F5',
+  secondary: '#4598F5',
   light: '#E5E5E5',
   grey: '#908e8c',
 };
