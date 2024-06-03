f50f1a26464888a4625e64e33c888a032405375d
Gabriel Chioquetta <gabriel.obladen@gmail.com>
2024-06-03 17:12:26 -0300

    Update OnBoardScreen

--- src/views/screens/OnBoardScreen.tsx
+++ src/views/screens/OnBoardScreen.tsx
@@ -16,19 +16,20 @@ const OnBoardScreen: React.FC<OnBoardScreenProps> = ({ navigation }) => {
           style={{
             width: '100%',
             resizeMode: 'contain',
-            top: -150,
+            top: -200,
           }}
           source={require('../../assets/onboardImage.png')}
         />
       </View>
       <View style={style.textContainer}>
         <View>
-          <Text style={{ fontSize: 32, fontWeight: 'bold', textAlign: 'center' }}>
+          <Text style={{ top: -90, fontSize: 32, fontWeight: 'bold', textAlign: 'center' }}>
             Comida Deliciosa
           </Text>
           <Text
             style={{
-              marginTop: 20,
+              top: -90,
+              marginTop: 10,
               fontSize: 18,
               textAlign: 'center',
               color: COLORS.grey,
@@ -36,12 +37,7 @@ const OnBoardScreen: React.FC<OnBoardScreenProps> = ({ navigation }) => {
             Nós te ajudamos a encontrar a melhor e mais deliciosa comida
           </Text>
         </View>
-        <View style={style.indicatorContainer}>
-          <View style={style.currentIndicator} />
-          <View style={style.indicator} />
-          <View style={style.indicator} />
-        </View>
-        <PrimaryButton
+        <PrimaryButton 
           onPress={() => navigation.navigate('HomeScreen')}
           title="Começar"
         />
@@ -56,28 +52,7 @@ const style = StyleSheet.create({
     paddingHorizontal: 50,
     justifyContent: 'space-between',
     paddingBottom: 40,
-  },
-  indicatorContainer: {
-    height: 50,
-    flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  currentIndicator: {
-    height: 12,
-    width: 30,
-    borderRadius: 10,
-    backgroundColor: COLORS.primary,
-    marginHorizontal: 5,
-  },
-  indicator: {
-    height: 12,
-    width: 12,
-    borderRadius: 6,
-    backgroundColor: COLORS.grey,
-    marginHorizontal: 5,
-  },
+  }
 });
 
 export default OnBoardScreen;
